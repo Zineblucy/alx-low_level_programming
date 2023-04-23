68431cf51b737e6679db3f8cecd683df81f9387b
Zineblucy <moutahaddir.zineb@gmail.com>
2023-04-23 14:18:45 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,26 +6,29 @@
  */
 int main(void)
 {
-int n, m, l;
+	int ones = '0';
+	int tens = '0';
+	int hundreds = '0';
 
-	for (n = 48; n < 58; n++)
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
-		for (m = 49; m < 58; n++)
+		for (tens = '0'; tens <= '9'; tens++)
 		{
-			for (l = 50; l < 58; l++)
+			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (l > m && m > n)
+				if (!((ones == tens) || (tens == hundreds) ||
+							(tens > ones) || (hundreds > tens))) /*eliminates repitition*/
 				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9' && hundreds == '7' &&
+								tens == '8')) /*adds commas and spaces*/
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
-
 			}
 		}
 	}
