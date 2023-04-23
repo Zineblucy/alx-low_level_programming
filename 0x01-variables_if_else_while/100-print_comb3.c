8234b543abf581b482c77091bfc71c3f68adb50d
Zineblucy <moutahaddir.zineb@gmail.com>
2023-04-23 14:55:29 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,20 +3,23 @@
 /**
  * main - prints all possible different combinations of two digits.
  * Return: Always 0 (Success)
+ *
  */
+
 int main(void)
+
 {
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= '9'; tens++)/*prints tens digit*/
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 	{
-		for (ones = '0'; ones <= '9'; ones++)/*prints ones digit*/
+		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
 		{
 			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(tens);
+				putchar(ones);
 				if (!(ones == '9' && tens == '8'))/*addes comma and space*/
 				{
 					putchar(',');
@@ -25,6 +28,9 @@ int main(void)
 			}
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
+
 }
